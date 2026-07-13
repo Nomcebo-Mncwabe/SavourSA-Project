f5ec538492928fab0e6e6056a159169ca4a743b0
Nomcebo Mncwabe <nomceboreyhana11@gmail.com>
2026-07-13 14:21:59 +0200

    Created admin module structure and dashboard layout

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SavourSA_Project.Controllers
+{
+    
+        public class AdminController : Controller
+        {
+            public ActionResult Dashboard()
+            {
+                return View();
+            }
+
+            public ActionResult Recipes()
+            {
+                return View();
+            }
+
+            public ActionResult Categories()
+            {
+                return View();
+            }
+
+            public ActionResult Users()
+            {
+                return View();
+            }
+
+            public ActionResult Reports()
+            {
+                return View();
+            }
+
+            public ActionResult Comments()
+            {
+                return View();
+            }
+
+            public ActionResult Settings()
+            {
+                return View();
+            }
+        }
+    }
