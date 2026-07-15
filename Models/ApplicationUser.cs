b074f1ae88ce212d59be4ade4ad49a2252ff138a
Siphilile0504 <sphililebuthelezi242@gmail.com>
2026-07-15 18:55:57 +0200

    Backend: Configure Identity, EF, Recipe and Category models

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SavourSA_Project.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+            Favourites = new HashSet<Favourite>();
+        }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(100)]
+        public string Province { get; set; }
+
+        public string ProfilePictureUrl { get; set; }
+
+        public bool AcceptedPopia { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public virtual ICollection<Favourite> Favourites { get; set; }
+    }
+}
---
+++ Models/Favourite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SavourSA_Project.Models
+{
+    public class Favourite
+    {
+        public int FavouriteId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        [Required]
+        public int RecipeId { get; set; }
+
+        public virtual Recipe Recipe { get; set; }
+
+        public DateTime DateSaved { get; set; } = DateTime.Now;
+    }
+}
